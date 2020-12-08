f6c370117dcef26568b2d02a04a9de68ce1165dd
mateja <mateja176@gmail.com>
2020-12-09 00:07:08 +0100

    formats file before output

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
 import * as fs from 'fs';
 import { join } from 'path';
+import prettier from 'prettier';
 import ts from 'typescript';
 
 const prog = ts.factory.createInterfaceDeclaration([], [], 'Test', [], [], []);
 
 const printer = ts.createPrinter();
 
+const source = printer.printNode(
+  ts.EmitHint.Unspecified,
+  prog,
+  ts.createSourceFile(
+    join(__dirname, '..', 'out', 'program.ts'),
+    '',
+    ts.ScriptTarget.Latest,
+  ),
+);
+
 fs.writeFile(
   join(__dirname, '..', 'out', 'program.ts'),
-  printer.printNode(
-    ts.EmitHint.Unspecified,
-    prog,
-    ts.createSourceFile(
-      join(__dirname, '..', 'out', 'program.ts'),
-      '',
-      ts.ScriptTarget.Latest,
-    ),
-  ),
+  prettier.format(source, { parser: 'typescript', singleQuote: true }),
   (err) => {
-    console.error(err);
+    if (err) {
+      console.error(err);
+    }
   },
 );
