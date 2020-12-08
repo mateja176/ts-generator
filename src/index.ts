67f31027d04dc38aa6c9d691ad1baa55c58e238d
mateja <mateja176@gmail.com>
2020-12-08 23:25:13 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+import { join } from 'path';
+import * as ts from 'typescript';
+
+const prog = ts.factory.createInterfaceDeclaration([], [], 'Test', [], [], []);
+
+const printer = ts.createPrinter();
+
+fs.writeFile(
+  join(__dirname, '..', 'out', 'program.ts'),
+  printer.printNode(
+    ts.EmitHint.Unspecified,
+    prog,
+    ts.createSourceFile(
+      join(__dirname, '..', 'out', 'program.ts'),
+      '',
+      ts.ScriptTarget.Latest,
+    ),
+  ),
+  (err) => {
+    console.error(err);
+  },
+);
